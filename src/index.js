ba97abfb75fd31b0b522887addbe2b1909eda99a
Alesia999 <73301282+Alesia999@users.noreply.github.com>
2021-03-08 15:26:59 +0100

    fix: solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined) return 0;
+  if(array.length === 0) return 0;
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined) return 0;
+  if(array.length === 0) return 0;
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined) return 0;
+  if(array.length === 0) return 0;
+  let sum = 0;
+  array.forEach(element => sum=sum+element);
+  return sum/(array.length);
 }
